534405d9c9458c94817dac20c19bbb7f3d42315e
Horacio Belardita <horaciobelardita@gmail.com>
2022-02-04 11:13:32 -0300

    add mongoose and integrating with mongo

--- index.js
+++ index.js
@@ -1,45 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-
+require('./mongo')
+const Note = require('./models/note')
+const unknownEndpoint = require('./middleware/notFound')
+const errorHandler = require('./middleware/errorHandler')
+const requestLogger = require('./middleware/requestLogger')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-
-const PORT = process.env.PORT || 3001
-
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
-
-function generateId() {
-  return notes.length > 0 ? Math.max(...notes.map((note) => note.id)) + 1 : 1
-}
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
 
 app.use(requestLogger)
 
@@ -50,7 +20,9 @@ app.get('/', (req, res) => {
 app
   .route('/api/notes')
   .get((req, res) => {
-    return res.json(notes)
+    Note.find().then((notes) => {
+      res.json(notes)
+    })
   })
 
   .post((req, res) => {
@@ -62,42 +34,62 @@ app
         error: `missing required fields (${validFields.join(',')})`
       })
     }
-    const id = generateId()
-    const note = {
-      id,
-      important: false,
-      date: new Date().toISOString(),
-      ...req.body
-    }
-    notes = [...notes, note]
-    return res.status(201).json(note)
+    const note = new Note({
+      content: req.body.content,
+      important: req.body.important || false,
+      date: new Date()
+    })
+    note.save().then((savedNote) => {
+      res.status(201).json(savedNote)
+    })
   })
 
 app
   .route('/api/notes/:id')
-  .get((req, res) => {
-    const id = +req.params.id
-    const note = notes.find((note) => note.id === id)
-    if (!note) {
-      return res.sendStatus(404)
-    }
-    return res.json(note)
+  .get((req, res, next) => {
+    const id = req.params.id
+    Note.findById(id)
+      .then((foundedNote) => {
+        if (!foundedNote) {
+          return res.sendStatus(404)
+        }
+        res.json(foundedNote)
+      })
+      .catch((err) => next(err))
   })
-  .delete((req, res) => {
-    const id = +req.params.id
-    const noteIndex = notes.findIndex((note) => note.id === id)
-    if (noteIndex === -1) {
-      return res.sendStatus(404)
-    }
-    notes.splice(noteIndex, 1)
-    return res.sendStatus(204)
+  .delete((req, res, next) => {
+    Note.findByIdAndRemove(req.params.id)
+      .then((deletedNote) => {
+        if (!deletedNote) {
+          return res.sendStatus(404)
+        }
+        return res.sendStatus(204)
+      })
+      .catch((error) => next(error))
   })
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  .put((req, res, next) => {
+    const dataToUpdate = {
+      content: req.body.content,
+      important: req.body.important
+    }
+    Note.findByIdAndUpdate(req.params.id, dataToUpdate, {
+      new: true,
+      runValidators: true
+    })
+      .then((returnedNote) => {
+        if (!returnedNote) {
+          return res.sendStatus(404)
+        }
+        return res.json(returnedNote)
+      })
+      .catch((error) => next(error))
+  })
 
 app.use(unknownEndpoint)
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
